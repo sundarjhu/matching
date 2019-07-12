5cf4b7cc8b0428383a047d7414c3c62ef951a727
Peter Scicluna <peterscicluna@asiaa.sinica.edu.tw>
2019-07-12 12:32:05 +0800

    Added second xmatch example that uploads a table for the query.

--- cdsxmatch.py
+++ cdsxmatch.py
@@ -7,6 +7,7 @@ from astroquery.vizier import Vizier
 
 if __name__ == "__main__":
     repeat_query=False
+    rereadvot=False
     if repeat_query:
 
         res = r.post('http://cdsxmatch.u-strasbg.fr/xmatch/api/v1/sync/tables',
@@ -34,7 +35,21 @@ if __name__ == "__main__":
                   #'colDec1': 'DEJ2000',
                   #'cols2':'2Mkey,d2M'
             },
-            #files={'cat1': open('posList.vot', 'r')}
+            #files={'cat1': f} #f needs to be a file object, i.e. f = open('filename', 'r')
+        )
+
+        res_upload = r.post(
+            'http://cdsxmatch.u-strasbg.fr/xmatch/api/v1/sync',
+            data={'request': 'xmatch',
+                  'distMaxArcsec': 30,
+                  'RESPONSEFORMAT': 'votable',
+                  #'cat1': 'vizier:II/338/catalog',
+                  'cat2': 'vizier:II/328/allwise'#,
+                  'colRA1': 'RAJ2000', #Change this to the name of the column that contains the the RA
+                  'colDec1': 'DEJ2000' #Change this to the name of the column that contains the the RA
+                  #'cols2':'2Mkey,d2M'
+            },
+            files={'cat1': f} #f needs to be a file object, i.e. f = open('filename', 'r') where filename contains the table you want to upload
         )
 
         #print(res)
@@ -42,18 +57,22 @@ if __name__ == "__main__":
         f = open("output.vot",'w')
         f.write(res.text)
         f.close()
-    
-    votable = parse('output.vot') #res.text)
-    print(votable)
-    #help(votable)
-    tablist = []
-    for resource in votable.resources:
-        for table in resource.tables:
-            tablist.append(table.to_table())
-            tablist[-1].pprint()
-            tablist[-1].write(str(table.ID)+'.vot',format="votable",overwrite=True)
-    #tab.pprint()
 
+    if rereadvot:
+        votable = parse('output.vot') #res.text)
+        print(votable)
+        #help(votable)
+        tablist = []
+        for resource in votable.resources:
+            for table in resource.tables:
+                tablist.append(table.to_table())
+                tablist[-1].pprint()
+                tablist[-1].write(str(table.ID)+'.vot',format="votable",overwrite=True)
+        #tab.pprint()
+        tab = tablist[-1]
+    else:
+        tab = Table.read('xmatchResult.vot',format="votable")
+        tab.pprint()
 
 
     ''' 
@@ -62,4 +81,11 @@ if __name__ == "__main__":
         which gives us the link to 2MASS. 
     '''
 
-    
+
+    ''' first parse the list of IDs into a single, very long, string '''
+    IDs = tab['ID'].data
+    print(IDs)
+    IDstring = str(IDs).replace('[ ','').replace('[','').replace(' ]','').replace(']','').replace('\n','').replace('  ',' ').replace(' ',',')
+    print(IDstring)
+    vizier = Vizier.query_constraints(catalog="II/328/allwise", ID='='+IDstring)
+    vizier.pprint()
